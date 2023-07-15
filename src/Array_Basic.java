73b4f8a474c0e0efd526bfd826589faf26aa865b
[priyanka7123] <priyankadas.7123@gmail.com>
2023-07-15 14:34:59 +0530

    DAY1

---
+++ src/Array_Basic.java
@@ -0,0 +1,17 @@
+
+public class Array_Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int[] age = {10,20,40,60};
+		for(int i=0; i<=4;i++) {
+			System.out.println(age[i]);
+		}
+
+	}
+
+}
+
+
